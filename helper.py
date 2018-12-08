ca7bac90ef1ab9141f249af1fd2d5d64a32ae46c
agent49 <kai.jurgeit@t-online.de>
2018-12-08 15:40:42 +0100

    next_batch working

--- helper.py
+++ helper.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def load_data(input_dir, fileprefix='table_of_drirs_100-0',
               n_files=1, file_size=100, width=38, height=1400, cut_start=600, cut_end=2000):
+    """Load data["features"] and data["labels"] from DRIR tables as mat-files from an input directory"""
+
     n_data = n_files * file_size
     data = {
         "features": np.zeros((n_data, width, height)),
@@ -14,7 +16,6 @@ def load_data(input_dir, fileprefix='table_of_drirs_100-0',
     for l in range(0, n_files):
         path_data = os.path.join(input_dir, fileprefix + str(l) + '.mat')
         mat = scipy.io.loadmat(path_data)
-        print("i: ", l, "path_data: ", path_data)
 
         # Extract data and labels from mat-File
         for k in range(0, file_size):
@@ -28,12 +29,28 @@ def load_data(input_dir, fileprefix='table_of_drirs_100-0',
     return data
 
 
-# def split_data(data, split=0.8):
-#     split = 0.8
-#     x_train = data["features"][:int(n_data * split), :, :, :]
-#     y_train = data["labels"][:int(n_data * split), :]
-#
-#     x_test = data["features"][int(n_data * split):, :, :, :]
-#     y_test = data["labels"][int(n_data * split):, :]
-#
-#     return x_train, y_train, x_test, y_test
+def split_data(data, split=0.8):
+    """Splits data into training and test given a value from 0 to 1"""
+    n_data = data["features"].shape[0]
+    x_train = data["features"][:int(n_data * split), :, :, :]
+    y_train = data["labels"][:int(n_data * split), :]
+
+    x_test = data["features"][int(n_data * split):, :, :, :]
+    y_test = data["labels"][int(n_data * split):, :]
+
+    return x_train, y_train, x_test, y_test
+
+
+def next_batch(batch_size, x, y):
+    """Returns batches of features and labels"""
+    if len(x) <= (next_batch.pointer + batch_size - 1):
+        x_batch = x[next_batch.pointer:, :, :, :]
+        y_batch = y[next_batch.pointer:, :]
+    else:
+        x_batch = x[next_batch.pointer:(next_batch.pointer + batch_size), :, :, :]
+        y_batch = y[next_batch.pointer:(next_batch.pointer + batch_size), :]
+    next_batch.pointer += batch_size
+    return x_batch, y_batch
+
+
+next_batch.pointer = 0
